c19bbec5890c68f9409d5a373d84d30b7b5e0411
Christopher Kaster <ikasoki@gmail.com>
2013-04-18 10:14:10 +0200

    fixed several streets

--- src/de/kasoki/swtrealtime/BusTime.java
+++ src/de/kasoki/swtrealtime/BusTime.java
@@ -56,7 +56,7 @@ public class BusTime implements Comparable<BusTime> {
 	
 	private static final int MAJOR_VERSION = 0;
 	private static final int MINOR_VERSION = 1;
-	private static final int PATCH_VERSION = 7;
+	private static final int PATCH_VERSION = 8;
 	
 	static {
 		dateFormat = new SimpleDateFormat("HH:mm");
