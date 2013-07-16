a590557b47f25dfedc0e2a0aeb8933a364901874
Christopher Kaster <ikasoki@gmail.com>
2013-07-16 19:25:15 +0200

    add new exceptions

--- src/de/kasoki/swtrealtime/BusTime.java
+++ src/de/kasoki/swtrealtime/BusTime.java
@@ -23,22 +23,20 @@
 // THE SOFTWARE.
 package de.kasoki.swtrealtime;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
+import de.kasoki.swtrealtime.exceptions.InvalidResponseException;
+import de.kasoki.swtrealtime.exceptions.ServerResponseException;
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import java.io.*;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.net.URLConnection;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Date;
 import java.util.List;
-
-import org.json.JSONArray;
-import org.json.JSONException;
 
 /**
  * Get real time information from SWT. Neither this library nor the author are related to SWT.
@@ -132,7 +130,7 @@ public class BusTime implements Comparable<BusTime> {
 	 * @param busStop The bus stop of interest ;).
 	 * @return
 	 */
-	public static List<BusTime> fromStopCode(BusStop busStop) {
+	public static List<BusTime> fromStopCode(BusStop busStop) throws ServerResponseException {
 		return BusTime.fromStopCode(busStop.getStopCode());
 	}
 	
@@ -141,99 +139,117 @@ public class BusTime implements Comparable<BusTime> {
 	 * @param stopCode The stop code of your bus stop.
 	 * @return
 	 */
-	public static List<BusTime> fromStopCode(String stopCode) {
-		String url = "http://212.18.193.124/onlineinfo/onlineinfo/stopData";
-		String charset = "UTF-8";
-		
-		String response = "";
-		
-		List<BusTime> busTimeList = null;
-		
-		try {
-			// setup connection
-			URLConnection connection = new URL(url).openConnection();
-			connection.setDoOutput(true); // triggers POST
-			
-			connection.setRequestProperty("Accept-Charset", charset);
-			connection.setRequestProperty("X-GWT-Module-Base", "http://212.18.193.124/onlineinfo/onlineinfo/");
-			connection.setRequestProperty("X-GWT-Permutation", "D8AB656D349DD625FC1E4BA18B0A253C");
-			connection.setRequestProperty("Content-Type", "text/x-gwt-rpc; charset=" + charset);
-			
-			String body = "5|0|6|http://212.18.193.124/onlineinfo/onlineinfo/|7E201FB9D23B0EA0BDBDC82C554E92FE|com.initka.onlineinfo.client.services.StopDataService|getDepartureInformationForStop|java.lang.String/2004016611|%s|1|2|3|4|1|5|6|";
-			body = String.format(body, stopCode);
-			
-			OutputStream output = connection.getOutputStream();
-			output.write(body.getBytes());
-			output.close();
-			
-			connection.connect();
-			
-			// read connection
-			InputStream input = connection.getInputStream();
-			
-			BufferedReader reader = new BufferedReader(new InputStreamReader(input, charset));
-			
-			for (String line; (line = reader.readLine()) != null;) {
-	            response += line;
-	        }
-			
-			reader.close();
-			
-			busTimeList = BusTime.parseResponse(response);
-			
-		} catch (MalformedURLException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		
-		return busTimeList;
-	}
-	
-	private static List<BusTime> parseResponse(String response) throws Exception {
-		List<BusTime> busTimeList = new ArrayList<BusTime>();
-		
-		if(response.startsWith("//OK")) {
-			String jsonString = response.substring(4, response.length());
-			
-			try {
-				JSONArray json = new JSONArray(jsonString);
-				
-				JSONArray innerInformations = new JSONArray(json.get(json.length() - 3).toString());
-				
-				for(int i = 0; i < Math.floor(json.length() / 11); i++) {
-					
-					String numberString = getItemFromInnerInformationList(innerInformations, json.getInt(i * 11 + 5));
-					
-					try {
-						int number = Integer.parseInt(numberString);
-						String destination = getItemFromInnerInformationList(innerInformations, json.getInt(i * 11 + 6));
-						Date arrivalTime = new Date(json.getLong(i * 11 + 2) + json.getLong(i * 11 + 3));
-						Date expectedArrivalTime = new Date(json.getLong(i * 11 + 7) + json.getLong(i * 11 + 8));
-						
-						busTimeList.add(new BusTime(number, destination, arrivalTime, expectedArrivalTime));
-					} catch(Exception ex) {
-						// if this happens we've found a bus without destination or number... don't know what they
-						// are but they actually exist! Oo
-						continue;
-					}
-					
-				}
-			} catch(JSONException e) {
-				e.printStackTrace();
-			}
-		} else {
-			throw new Exception("Error: Invalid Response: " + response);
-		}
-		
-		return busTimeList;
-	}
-	
-	private static String getItemFromInnerInformationList(JSONArray innerInformations, int index) {
-		return innerInformations.getString(index - 1);
-	}
+    public static List<BusTime> fromStopCode(String stopCode) throws ServerResponseException {
+        String url = "http://212.18.193.124/onlineinfo/onlineinfo/stopData";
+        String charset = "UTF-8";
+
+        String response = "";
+
+        List<BusTime> busTimeList = null;
+
+        try {
+            // setup connection
+            HttpURLConnection connection = (HttpURLConnection) new URL(url).openConnection();
+            connection.setDoOutput(true); // triggers POST
+
+            connection.setRequestProperty("Accept-Charset", charset);
+            connection.setRequestProperty("X-GWT-Module-Base", "http://212.18.193.124/onlineinfo/onlineinfo/");
+            connection.setRequestProperty("X-GWT-Permutation", "D8AB656D349DD625FC1E4BA18B0A253C");
+            connection.setRequestProperty("Content-Type", "text/x-gwt-rpc; charset=" + charset);
+
+            String body = "5|0|6|http://212.18.193.124/onlineinfo/onlineinfo/|7E201FB9D23B0EA0BDBDC82C554E92FE|com.initka.onlineinfo.client.services.StopDataService|getDepartureInformationForStop|java.lang.String/2004016611|%s|1|2|3|4|1|5|6|";
+            body = String.format(body, stopCode);
+
+            OutputStream output = connection.getOutputStream();
+            output.write(body.getBytes());
+            output.close();
+
+            handleHTTPResponse(connection.getResponseCode());
+
+            connection.connect();
+
+            // read connection
+            InputStream input = connection.getInputStream();
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(input, charset));
+
+            for (String line; (line = reader.readLine()) != null;) {
+                response += line;
+            }
+
+            reader.close();
+
+            busTimeList = BusTime.parseResponse(response);
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InvalidResponseException e) {
+            e.printStackTrace();
+        }
+
+        return busTimeList;
+    }
+
+    private static List<BusTime> parseResponse(String response) throws InvalidResponseException {
+        List<BusTime> busTimeList = new ArrayList<BusTime>();
+
+        if(response.startsWith("//OK")) {
+            String jsonString = response.substring(4, response.length());
+
+            try {
+                JSONArray json = new JSONArray(jsonString);
+
+                JSONArray innerInformations = new JSONArray(json.get(json.length() - 3).toString());
+
+                for(int i = 0; i < Math.floor(json.length() / 11); i++) {
+
+                    String numberString = getItemFromInnerInformationList(innerInformations, json.getInt(i * 11 + 5));
+
+                    try {
+                        int number = Integer.parseInt(numberString);
+                        String destination = getItemFromInnerInformationList(innerInformations, json.getInt(i * 11 + 6));
+                        Date arrivalTime = new Date(json.getLong(i * 11 + 2) + json.getLong(i * 11 + 3));
+                        Date expectedArrivalTime = new Date(json.getLong(i * 11 + 7) + json.getLong(i * 11 + 8));
+
+                        busTimeList.add(new BusTime(number, destination, arrivalTime, expectedArrivalTime));
+                    } catch(Exception ex) {
+                        // if this happens we've found a bus without destination or number... don't know what they
+                        // are but they actually exist! Oo
+                        continue;
+                    }
+
+                }
+            } catch(JSONException e) {
+                e.printStackTrace();
+            }
+        } else {
+            throw new InvalidResponseException("Error: Invalid Response: " + response);
+        }
+
+        return busTimeList;
+    }
+
+    public static void handleHTTPResponse(int responseCode) throws ServerResponseException {
+
+        switch(responseCode) {
+            case 500:
+                throw new ServerResponseException("Internal Server Error", 500);
+            case 501:
+                throw new ServerResponseException("The request cannot be carried out by the server.", 501);
+            case 502:
+                throw new ServerResponseException("Bad Gateway", 502);
+            case 503:
+                throw new ServerResponseException("Temporarily Unavailable", 503);
+            case 504:
+                throw new ServerResponseException("The gateway has timed out", 504);
+        }
+    }
+
+    private static String getItemFromInnerInformationList(JSONArray innerInformations, int index) throws JSONException {
+        return innerInformations.getString(index - 1);
+    }
 	
 	public static BusTime getDummy() {
 		return new BusTime(1337, "Invalid Bus stop", new Date(System.currentTimeMillis()), new Date(System.currentTimeMillis()));
@@ -248,9 +264,16 @@ public class BusTime implements Comparable<BusTime> {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		List<BusTime> list = BusTime.fromStopCode(BusStop.HAUPTBAHNHOF);
-		
-		Collections.sort(list);
+        List<BusTime> list = null;
+
+        try {
+            list = BusTime.fromStopCode(BusStop.HAUPTBAHNHOF);
+        } catch (ServerResponseException e) {
+            System.out.println(e.getServerResponseCode());
+            e.printStackTrace();
+        }
+
+        Collections.sort(list);
 		
 		for(BusTime busTime : list) {
 			System.out.println(busTime);
